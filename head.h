ed2dddfe05d9acf1fa1a07f93d8d0eb009b47fd3
Taliens <just4fun2048@gmail.com>
2015-04-19 11:26:14 +0800

    myhead

---
+++ head.h
@@ -0,0 +1,15 @@
+#ifndef _HEAD_H_
+#define _HEAD_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#endif
