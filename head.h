269be35b2e4d037a6d096fa971adfbf3b2e0c48c
Taliens <just4fun2048@gmail.com>
2015-04-20 20:24:54 +0800

    add share memory

--- head.h
+++ head.h
@@ -2,14 +2,20 @@
 #define _HEAD_H_
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <signal.h>
+
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/msg.h>
 
 #endif
